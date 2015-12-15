4ada267ff708d75b90c077c5fa32881a80f500af
smh3005 <smh3005@gmail.com>
2015-12-15 14:24:44 -0600

    Initial Commit
    Adds TankDriveTeleopMode2 from ftc_app-master_S1.

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
@@ -55,20 +55,20 @@ public class FtcOpModeRegister implements OpModeRegister {
      * If two or more op modes are registered with the same name, the app will display an error.
      */
 
-    manager.register("NullOp", NullOp.class);
+    /*manager.register("NullOp", NullOp.class);
 
     //manager.register("MatrixK9TeleOp", MatrixK9TeleOp.class);
     manager.register("K9TeleOp", K9TeleOp.class);
     manager.register("K9Line", K9Line.class);
     manager.register ("PushBotAuto", PushBotAuto.class);
     manager.register ("PushBotManual", PushBotManual.class);
-
+    */
 
 
     /*
      * Uncomment any of the following lines if you want to register an op mode.
      */
-    manager.register("MR Gyro Test", MRGyroTest.class);
+    //manager.register("MR Gyro Test", MRGyroTest.class);
 
     //manager.register("AdafruitRGBExample", AdafruitRGBExample.class);
     //manager.register("ColorSensorDriver", ColorSensorDriver.class);
@@ -96,5 +96,7 @@ public class FtcOpModeRegister implements OpModeRegister {
     //manager.register("PushBotDriveTouch", PushBotDriveTouch.java);
     //manager.register("PushBotIrSeek", PushBotIrSeek.java);
     //manager.register("PushBotSquare", PushBotSquare.java);
+
+    manager.register("TankDriveArmTeleopMode2", TankDriveArmTeleopMode2.class);
   }
 }
---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TankDriveArmTeleopMode2.java
@@ -0,0 +1,146 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+import com.qualcomm.ftccommon.DbgLog;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorController;
+import com.qualcomm.robotcore.util.Range;
+
+/**
+ * Created by smh30 on 12/8/2015.
+ */
+public class TankDriveArmTeleopMode2 extends OpMode {
+
+    DcMotor leftDrive;
+    DcMotor rightDrive;
+    DcMotor shoulder;
+    DcMotor elbow;
+
+    double leftY = 1/3;
+    double rightY = 1/3;
+
+    double shoulderPower;
+    double elbowPower;
+
+
+    @Override
+    public void init() {
+
+        //get references to the hardware from the hardware map
+        this.leftDrive = hardwareMap.dcMotor.get("leftDrive");
+        this.rightDrive = hardwareMap.dcMotor.get("rightDrive");
+
+        this.shoulder = hardwareMap.dcMotor.get("shoulder");
+        this.elbow = hardwareMap.dcMotor.get("elbow");
+
+        // I essentially copied this from SynchTeleOp
+        // Configure the knobs of the hardware according to how you've wired your
+        // robot. Here, we assume that there are no encoders connected to the motors,
+        // so we inform the motor objects of that fact.
+        this.leftDrive.setMode(DcMotorController.RunMode.RUN_WITHOUT_ENCODERS);
+        this.rightDrive.setMode(DcMotorController.RunMode.RUN_WITHOUT_ENCODERS);
+        this.shoulder.setMode(DcMotorController.RunMode.RUN_WITHOUT_ENCODERS);
+        this.elbow.setMode(DcMotorController.RunMode.RUN_WITHOUT_ENCODERS);
+
+    }
+
+    @Override
+    public void loop() {
+
+        if (gamepad1.left_bumper) {
+            try {
+                leftY = Range.clip(leftY * 3, -1, 1);
+            } catch (Exception e) {
+                this.leftDrive.setPower(0);
+                DbgLog.msg("SMH - Left Bumper got an error");
+            }
+        } else {
+            try {
+                leftY = Range.clip(this.gamepad1.left_stick_y / 3, -1, 1);
+            } catch (Exception e) {
+                this.leftDrive.setPower(0);
+                DbgLog.msg("SMH - No Left Bumper got an error");
+            }
+        }
+        if (gamepad1.right_bumper) {
+            try {
+                rightY = Range.clip(rightY * 3, -1, 1);
+            } catch (Exception e) {
+                this.rightDrive.setPower(0);
+                DbgLog.msg("SMH - Right Bumper got an error");
+            }
+
+        } else {
+            try {
+                rightY = Range.clip(this.gamepad1.right_stick_y / 3, -1, 1);
+            } catch (Exception e) {
+                this.rightDrive.setPower(0);
+                DbgLog.msg("SMH - No Right Bumper got an error");
+            }
+
+        }
+
+        //set the power of the motors with the gamepad values
+        try {
+            this.leftDrive.setPower(leftY);
+        } catch (Exception e) {
+            this.leftDrive.setPower(0);
+            DbgLog.msg("SMH - Setting the leftDrive power got an error");
+        }
+
+        try {
+            this.rightDrive.setPower(-rightY);
+        } catch (Exception e) {
+            this.rightDrive.setPower(0);
+            DbgLog.msg("SMH - Setting the rightDrive power got an error");
+        }
+
+        try {
+            shoulderPower = (this.gamepad2.left_stick_y/4);
+        } catch (Exception e){
+            this.shoulder.setPower(0);
+            DbgLog.msg("SMH - Setting shoulderPower got an error");
+        }
+
+        try {
+            elbowPower = (this.gamepad2.right_stick_y/2);
+        } catch (Exception e) {
+            this.elbow.setPower(0);
+            DbgLog.msg("SMH - Setting elbowPower got an error");
+        }
+
+        //Manipulate the arm;
+        try {
+            this.shoulder.setPower(-shoulderPower);
+        } catch (Exception e) {
+            this.shoulder.setPower(0);
+            DbgLog.msg("SMH - Setting shoulder's power to shoulderPower got an error");
+        }
+
+        try {
+            this.elbow.setPower(-elbowPower);
+        } catch (Exception e) {
+            this.elbow.setPower(0);
+            DbgLog.msg("SMH - Setting elbow's power to elbowPower got an error");
+        }
+
+        try {
+            telemetry.addData("rightDrive: ", rightY);
+            telemetry.addData("leftDrive: ", leftY);
+            telemetry.addData("shoulder: ", shoulderPower);
+            telemetry.addData("elbow: ", elbowPower);
+        } catch (Exception e) {
+            DbgLog.msg("Using telemetry.addData got an error");
+        }
+
+
+    }
+    @Override
+    public void stop() {
+        this.rightDrive.setPower(0);
+        this.leftDrive.setPower(0);
+        this.shoulder.setPower(0);
+        this.elbow.setPower(0);
+
+    }
+}
